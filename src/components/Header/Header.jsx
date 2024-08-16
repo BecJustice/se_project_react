0e5eda2621443fe18d4228a76eccdbe1ffbcd457
rebecca justice <rebecca.bindraban@gmail.com>
2024-08-16 17:33:17 -0600

    started header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <header className="header">
+      <img className="header__logo" src="" alt="" />
+      <p className="header__date-and-location">DATE, LoOCATTION</p>
+      <button className="header__add-clothes-btn">+ Add Clothes</button>
+      <div className="header__user-container">
+        <p className="header__username">NAME</p>
+        <img src="" alt="" className="header__avatar" />
+      </div>
+    </header>
+  );
+}
+
+// at 15:20 on header video
+export default Header;
